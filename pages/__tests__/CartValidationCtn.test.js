a4c402a3b2c5d8a50a21a59a904d29782d846ca4
denis-fegnoux <denis.fegnoux@schibsted.com>
2018-12-28 15:07:28 +0100

    refactor(CartValidationCtn): Remove unsed variables

--- pages/__tests__/CartValidationCtn.test.js
+++ pages/__tests__/CartValidationCtn.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { shallow } from "enzyme";
-import renderer from "react-test-renderer";
 import CartValidationCtn from "../CartValidationCtn";
 
 describe("CartValidationCtn lifecylce", () => {
